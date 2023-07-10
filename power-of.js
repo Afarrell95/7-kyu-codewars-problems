0df9129d02e553107bb7b875a0f56faf693d93f0
Allison01~ <farrella95@gmail.com>
2023-07-10 18:04:20 -0400

    new prob

---
+++ power-of.js
@@ -0,0 +1,23 @@
+// Instructions
+// Output
+// Write a method that returns true if a given parameter is a power of 4, and false if it's not. If parameter is not an Integer (eg String, Array) method should return false as well.
+
+// (In C# Integer means all integer Types like Int16,Int32,.....)
+
+// Examples
+// powerOf4(1024) // returns true
+// powerOf4(44) // returns false
+// powerOf4("not a positive integer") // returns false
+
+
+
+
+function powerOf4(n) {
+    if(typeof n !== "number"){
+      return false
+    }else if(Math.log2(n) % 2 === 0){
+      return true
+    }else{
+      return false
+    }
+  }
